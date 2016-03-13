33d8441ce827f3dda4303c78789b6ad57d859896
christianesperar <christianesperar@gmail.com>
2016-03-13 18:29:54 +0800

    Refactor script that result to remove some glitch where hide most recent element is still showing when opening multiple tab at once and when chrome first startup

--- background.js
+++ background.js
@@ -1,16 +1,24 @@
-processTabAction = function ( tab ) {
+var urls = [
+  "chrome://newtab/", 
+  "chrome-search://local-ntp/local-ntp.html"
+];
+
+var sendTabMessage = function (tab) {
+  if(urls.indexOf(tab.url) != -1) {
+    chrome.tabs.sendMessage(tab.id, "activate");
+  }
+};
+
+var onTabShow = function (tabId, changeInfo, tab) {
+  if (changeInfo.status === "complete") {
+    sendTabMessage(tab);
+  }
+};
+
+chrome.tabs.onUpdated.addListener(onTabShow);
+
+document.addEventListener("DOMContentLoaded", function(event) { 
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    if ( tabs && tabs[0] && 
-        (tabs[0].url == "chrome://newtab/" || 
-         tabs[0].url == "chrome-search://local-ntp/local-ntp.html") ) {
-      chrome.tabs.sendMessage(tabs[0].id, "activate");
-    }
+    sendTabMessage(tabs[0]);
   });
-};
-
-handleTabUrlChange = function ( tabId, changeInfo, tab ) {
-  processTabAction(tab);
-};
-
-chrome.tabs.onUpdated.addListener(handleTabUrlChange);
-chrome.webNavigation.onDOMContentLoaded.addListener(handleTabUrlChange);
+});
