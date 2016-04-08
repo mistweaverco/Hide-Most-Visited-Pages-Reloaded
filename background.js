46419d7a4d001c0ded65fd5a5a9d85e61abaf06d
christianesperar <christianesperar@gmail.com>
2016-04-08 17:38:09 +0800

    Optimize code

--- background.js
+++ background.js
@@ -1,16 +1,16 @@
 var urls = [
-  "chrome://newtab/", 
-  "chrome-search://local-ntp/local-ntp.html"
+  'chrome://newtab/', 
+  'chrome-search://local-ntp/local-ntp.html'
 ];
 
 var sendTabMessage = function (tab) {
   if(urls.indexOf(tab.url) != -1) {
-    chrome.tabs.sendMessage(tab.id, "activate");
+    chrome.tabs.sendMessage(tab.id, 'activate');
   }
 };
 
 var onTabShow = function (tabId, changeInfo, tab) {
-  if (changeInfo.status === "complete") {
+  if (changeInfo.status === 'complete') {
     sendTabMessage(tab);
   }
 };
@@ -21,15 +21,15 @@ var onDOMContentLoaded = function () {
   });
 };
 
-// Remove HMVPR when creating new tab
+// Remove HMVP when creating new tab
 chrome.tabs.onUpdated.addListener(onTabShow);
 
-// Remove HMVPR when refreshing the new tab page
+// Remove HMVP when refreshing the new tab page
 chrome.webNavigation.onDOMContentLoaded.addListener(function(event) {
   onDOMContentLoaded();
 });
 
-// Remove HMVPR on initial startup
-document.addEventListener("DOMContentLoaded", function(event) {
+// Remove HMVP on initial startup
+document.addEventListener('DOMContentLoaded', function(event) {
   onDOMContentLoaded();
 });
--- hideRecents.js
+++ hideRecents.js
@@ -1,24 +1,27 @@
 chrome.runtime.onMessage.addListener(function(msg) {
-  if ( msg === 'activate' ) {
+  if (msg === 'activate') {
     try {
       document.getElementById('most-visited').style.display = 'none';
     } catch ( e ) {}
-  chrome.storage.sync.get(['cbAlsoHideLogo','cbAlsoHideSearchbar', 'cbAlsoHideNavbar'], function( obj ) {
-    if ( obj.cbAlsoHideLogo && obj.cbAlsoHideLogo === true ) {
-      try {
-        document.getElementById('lga').style.visibility = 'hidden';
-      } catch ( e ) {}
-    }
-    if ( obj.cbAlsoHideSearchbar && obj.cbAlsoHideSearchbar === true ){
-      try {
-        document.getElementById('f').style.display = 'none';
-      } catch( e ) {}
-    }
-    if ( obj.cbAlsoHideNavbar && obj.cbAlsoHideNavbar === true ) {
-      try {
-        document.getElementById('mngb').style.display = 'none';
-      } catch( e ) {}
-    }
-  });
+
+    chrome.storage.sync.get(['cbAlsoHideLogo', 'cbAlsoHideSearchbar', 'cbAlsoHideNavbar'], function( obj ) {
+      if (obj.cbAlsoHideLogo) {
+        try {
+          document.getElementById('lga').style.visibility = 'hidden';
+        } catch ( e ) {}
+      }
+
+      if (obj.cbAlsoHideSearchbar) {
+        try {
+          document.getElementById('f').style.display = 'none';
+        } catch( e ) {}
+      }
+
+      if (obj.cbAlsoHideNavbar) {
+        try {
+          document.getElementById('mngb').style.display = 'none';
+        } catch( e ) {}
+      }
+    });
   }
 });
